85dcfc606c808134cf336938aab225364a5539a2
Hari Chaitanya <24608554+Harikodapaka@users.noreply.github.com>
2025-10-09 12:59:11 -0400

    update logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,16 +13,22 @@ export default function Header() {
 
     return (<header className="bg-white border-b border-[#e9e9e9] dark:bg-black dark:border-gray-700">
         <div className="mx-auto flex h-16 max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8">
-            <Link className="block text-teal-600" href="/">
+            <Link className="block text-teal-600 flex items-center gap-2" href="/">
                 <span className="sr-only">Home</span>
                 <Image
                     className="dark:invert"
-                    src="/next.svg"
-                    alt="Next.js logo"
-                    width={120}
-                    height={38}
+                    src="/cricket.png"
+                    alt="Cricket logo"
+                    width={45}
+                    height={45}
                     priority
                 />
+                <div className='flex flex-col leading-tight'>
+                    <h1 className="text-lg font-bold text-gray-900 dark:text-gray-100 sm:text-xl text-base">
+                        CricScore
+                    </h1>
+                    <span className='sm:inline hidden text-xs/2 text-gray-500 dark:text-gray-400'>Made for gully cricket</span>
+                </div>
             </Link>
             <div className='ml-auto flex gap-4 items-center'>
                 {pathname === '/' && <Link
--- src/components/Styles.ts
+++ src/components/Styles.ts
@@ -1,4 +1,4 @@
-export const BtnBase = `block rounded-md px-2 py-2 sm:px-5 sm:py-2.5 text-sm font-medium text-white transition cursor-pointer`;
+export const BtnBase = `block rounded-md px-2 py-2 sm:px-5 sm:py-2.5 text-xs font-medium text-white transition cursor-pointer sm:text-sm`;
 export const BlueBtn = `${BtnBase} bg-blue-500 hover:bg-blue-400`;
 export const OrangeBtn = `${BtnBase} bg-orange-500 hover:bg-orange-400`;
 export const BlueBtnOutlined = `inline-block rounded-sm border border-orange-600 px-12 py-3 text-sm font-medium text-orange-600 hover:bg-orange-600 hover:text-white focus:ring-3 focus:outline-hidden cursor-pointer`;
